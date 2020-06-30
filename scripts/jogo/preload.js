45ce3cc7291a321572c53306f0491a0d820f46d9
Saulo Ramos <57292499+Heremitta@users.noreply.github.com>
2020-06-30 15:39:31 -0300

    Update preload.js

--- scripts/jogo/preload.js
+++ scripts/jogo/preload.js
@@ -15,11 +15,11 @@ function preload(){
   imagemVida          = loadImage('imagens/assets/coracao.png');
   fontTelaInicial     = loadFont('imagens/assets/fonteTelaInicial.otf');
   somDoJogo           = createAudio('sons/trilha_jogo_top.mp3');
-  somDoJogo.volume(1);
+  somDoJogo.volume(5);
   somTelaInicial           = createAudio('sons/somTelaInicial.mp3');
-  somTelaInicial.volume(1);
+  somTelaInicial.volume(5);
   somGameOver           = createAudio('sons/gameOver.mp3');
-  somGameOver.volume(1);
+  somGameOver.volume(5);
 fita = loadJSON('fita/fita.json');
 }
   function getRndInteger(min, max) {
